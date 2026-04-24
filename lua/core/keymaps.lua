3f7e2c27914eed787646fd3f3fc24c5c91ab0784
takeuti-git <takeutiwastaken@gmail.com>
2026-04-24 15:45:33 +0900

    隠しファイルを含めたfindFilesキーを設定

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -35,6 +35,10 @@ map("n", "<leader>E", "<cmd>Ex<CR>")
 
 local builtin = require("telescope.builtin")
 map("n", "<leader>ff", builtin.find_files, { desc = "Find Files" })
+
+map("n", "<leader>faf", function()
+    builtin.find_files({ no_ignore = true, hidden = true })
+end, { desc = "Find files including gitignored and hidden" })
 map("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep" })
 map("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
 map("n", "<leader>fh", builtin.help_tags, { desc = "Help" })
